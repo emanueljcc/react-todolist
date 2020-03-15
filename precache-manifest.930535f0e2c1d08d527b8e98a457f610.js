99da7faf511f4f43ac2d660ad439968d5f9959da
Emanuel Castillo <emanuel.castillo@agence.cl>
2020-03-14 23:34:01 -0400

    Updates

---
+++ precache-manifest.930535f0e2c1d08d527b8e98a457f610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13da9f1a723e0fb68cac7625efd12dd",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "cb30bf361c83398f7738",
+    "url": "/react-todolist/static/css/main.182abb49.chunk.css"
+  },
+  {
+    "revision": "082105b3ef2b6a4fe343",
+    "url": "/react-todolist/static/js/2.840d5a42.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-todolist/static/js/2.840d5a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb30bf361c83398f7738",
+    "url": "/react-todolist/static/js/main.967176e0.chunk.js"
+  },
+  {
+    "revision": "9a2116910dc5f2de30f8",
+    "url": "/react-todolist/static/js/runtime-main.2621db2d.js"
+  }
+]);
